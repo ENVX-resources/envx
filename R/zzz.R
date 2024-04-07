1b50b0f74fcbfe6bbf849675452436db37f0ac01
Januar Harianto <januar.harianto@sydney.edu.au>
2024-04-08 00:18:09 +1000

    optimise startup code

--- R/zzz.R
+++ R/zzz.R
@@ -1,27 +1,29 @@
-mclustStartupMessage <- function()
-{
+startupmessage <- function() {
   # Startup message obtained as
   # > figlet -f slant MCLUST
-  msg <- c(paste0(
-    "\nWelcome to:\n",
-    " _____  _   _  _   _ __   __
+  msg <- c(
+    paste0(
+      "\nWelcome to:\n",
+      " _____  _   _  _   _ __   __
 |  ___|| \\ | || | | |\\ \\ / /
 | |__  |  \\| || | | | \\ V /
 |  __| | . ` || | | | /   \\
 | |___ | |\\  |\\ \\_/ // /^\\ \\
 \\____/ \\_| \\_/ \\___/ \\/   \\/  version ",
-    packageVersion("envx")),
-    "\n\nFor tutorials, type `list_tutorials()`\n")
+      utils::packageVersion("envx")
+    ),
+    "\n\nFor tutorials, type `list_tutorials()`\n"
+  )
   return(msg)
 }
 
-.onAttach <- function(lib, pkg)
-{
+.onAttach <- function(lib, pkg) {
   # unlock .mclust variable allowing its modification
   # startup message
-  msg <- mclustStartupMessage()
-  if(!interactive())
+  msg <- startupmessage()
+  if (!interactive()) {
     msg[1] <- paste("Package 'envx' version", packageVersion("envx"))
+  }
   packageStartupMessage(msg)
   invisible()
 }
